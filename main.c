82dbe6e11be0d8fae703cf772b843ead71e0c47f
192210671 <124900140+192210671@users.noreply.github.com>
2023-08-08 17:27:46 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+
+	int a=0,b=1,c;
+	int sum=0;
+	int i,n;
+    printf("enter the range \n");
+	scanf("%d",&n);
+
+	for(i=1;i<=n;i++)
+	{
+		 c=a+b;
+		 printf("%d\n",a);
+		 if(i%2==0)
+		 {
+
+		 	sum=sum+a;
+		 }
+
+		 a=b;
+		 b=c;
+
+
+	}
+
+	printf("sum of digits in even places is: %d\n",sum);
+
+
+
+return 0;
+
+}
